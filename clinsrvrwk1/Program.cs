1d1c2b0606f3181a7a2b48014e05b0761b52029f
000450745 <000450745@CC145XX1A010XXX>
2023-05-05 19:05:36 -0700

    Add project files.

---
+++ clinsrvrwk1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace clinsrvrwk1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from 1303!");
+        }
+    }
+}
