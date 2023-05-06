ebaedd734f3261f1e573e1dec019e9e633260aba
rayna <rayna@DESKTOP-2GOJ6L0>
2023-05-06 11:03:08 -0700

    change something

---
+++ clinsrvrwk1/Point.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace clinsrvrwk1
+{
+    public class Point
+    {
+        public int x;
+        public int y;   
+    }
+}
