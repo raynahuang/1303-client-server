a2e13dc4d6d3240f1ae8a68a3176bbc5f82c3d39
000450745 <000450745@CC145XX1A010XXX>
2023-05-05 21:23:54 -0700

    save  the inital lesseon

--- clinsrvrwk1/Program.cs
+++ clinsrvrwk1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Http.Headers;
 
 namespace clinsrvrwk1
 {
@@ -6,7 +7,59 @@ namespace clinsrvrwk1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World from 1303!");
+            Console.WriteLine("Program was called with");
+            for(int i=0; i < args.Length ; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+
+            Program program = new Program();
+            program.PrintNumbers();
+
+
+            int inches =program.FeetToInches(13);
+            Console.WriteLine($"13 feet is: {inches} inches.");
+            string message = "this is our first program in C#";
+            var message2 = "new message";
+
+       
+            var arr = new int[] { 1, 2, 3, };
+            bool isThisGood = true;
+
+            var p1 = new Point();
+            p1.x = 10;
+            p1.y = 12;
+
+            var a = 5;
+            int b = 20;
+            b = a;          //copies the actual value from a to b
+
+
+            Point p2 = p1; //copies the reference from p1 into p2
+
         }
+
+
+        public void PrintNumbers()
+            {
+                Console.WriteLine("this is a test");
+
+                for (int i = 0; i < 1000; i++)
+                {
+                    Console.Write(" ");
+                    Console.Write(i);
+                }
+                Console.WriteLine("end of loop");
+
+
+            }
+
+        public int FeetToInches(int feet)
+        {
+            int inches = feet * 12;
+
+            return inches;
+        }
+        
     }
 }
