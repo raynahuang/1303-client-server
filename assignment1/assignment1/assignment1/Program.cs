92bd2a4625b020318221d53c31867838d392ff45
000450745 <000450745@CC145XX1A010XXX>
2023-05-06 13:26:35 -0700

    assignment1 new

---
+++ assignment1/assignment1/assignment1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice = GetUserChoice();
+            Console.WriteLine($"You selected option {choice}.");
+        }
+
+        public static int GetUserChoice()
+        {
+            int choice;
+
+            do
+            {
+                Console.WriteLine("Please select an option:");
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine($"{i} - Option {i}");
+                }
+
+                if (!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Invalid input. Please enter a number between 1 and 10.");
+                    continue;
+                }
+
+                if (choice < 1 || choice > 10)
+                {
+                    Console.WriteLine("Invalid input. Please enter a number between 1 and 10.");
+                    continue;
+                }
+
+                Console.WriteLine($"You selected Option {choice}");
+                break;
+
+            } while (true);
+
+            return choice;
+        }
+    }
+}
