aab5a959405539bec05e11444ef7d4da2e56ecf8
raynahuang <raynainca190217@gmail.com>
2023-05-12 20:20:16 -0700

    lab 3

---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace lab3
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            User[] users = new User[10];
+
+            for (int i = 0; i < users.Length; i++)
+            {
+                users[i] = new User
+                {
+                    ID = i + 1,
+                    Name = "placeholder" + (i + 1)
+                };
+            }
+
+            PrintUserArray(users);
+
+            Console.ReadLine();
+        }
+
+
+        public class User
+        {
+            public int ID;
+            public string Name;
+        }
+
+
+
+        static void PrintUserArray(User[] users)
+        {
+            foreach (User user in users)
+            {
+                Console.WriteLine($"ID: {user.ID}, Name: {user.Name}");
+            }
+        }
+
+
+
+    }
+}
