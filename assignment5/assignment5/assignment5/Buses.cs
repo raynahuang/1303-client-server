368fa8984cb01d97bd33d6bf80db83f8b1a7df33
Rayna Huang <raynainca190217@gmail.com>
2023-06-09 17:02:52 -0700

    add assignment 5

---
+++ assignment5/assignment5/assignment5/Buses.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment5
+{
+    public class Buses : Vehicle<VehicleType, Engine, Body>
+    {
+        public Buses(VehicleType type, Engine engine, Body body)
+            : base(type, engine, body)
+        {
+        }
+    }
+}
---
+++ assignment5/assignment5/assignment5/Cars.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment5
+{
+    public class Car : Vehicle<VehicleType, Engine, Body>
+    {
+        public Car(VehicleType type, Engine engine, Body body)
+            : base(type, engine, body)
+        {
+        }
+    }
+}
