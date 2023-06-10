8a2a7bce90bfb09a8e8c6c88f8acdb986e49ddce
Rayna Huang <raynainca190217@gmail.com>
2023-06-09 17:12:02 -0700

    add assignment5

---
+++ assignment5/assignment5/assignment5/Motocycles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment5
+{
+    public class Motorcycles : Vehicle<VehicleType, Engine, Body>
+    {
+        public Motorcycles(VehicleType type, Engine engine, Body body)
+            : base(type, engine, body)
+        {
+        }
+    }
+}
---
+++ assignment5/assignment5/assignment5/Trucks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment5
+{
+    public class Truck : Vehicle<VehicleType, Engine, Body>
+    {
+        public Truck(VehicleType type, Engine engine, Body body)
+            : base(type, engine, body)
+        {
+        }
+    }
+
+}
