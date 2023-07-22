794e76c2517e26e92c0095eb798faeddcf1d8a4d
Rayna Huang <raynainca190217@gmail.com>
2023-07-22 06:09:44 -0700

    improve assignment 8

--- assignment8/ContactManager/Contact.cs
+++ assignment8/ContactManager/Contact.cs
@@ -4,6 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+/*
+ Project Assignment			
+	Contact Manager		
+			
+	Command line application in C# to manage contacts.		
+	Contacts can be people, companies, ..		
+			
+	Operations		
+	Adding and removing contacts		
+	Searching by various criteria		
+	Save and load from disk		
+	Save and load from a remote server		
+			
+	Details		
+	A company may have a number of contacts		
+	There can be companies with no contacts 		
+	There can be individuals with no company information		
+	Contact details for individuals and companies are 		
+	Compnay: Name, Address, Phone numbers, emails		
+		Note that email and phone numbers may have a title (e.g. department) attached to it	
+		As discussed company may have a number of individuals with full contact information	
+	Individuals: Name, Address, Phone numbers, emails		
+		Note that email and phone numbers may have a title (e.g. work, home) attached to it	
+	Related contacts: Don't start until you have Part 1 and Part 2 are complated		
+		Both companies and inviduals can be related to other contacts or indiduals (more on this later)	
+			
+Part 1	Create the projects		
+	Create the models (models are the core classes and abstractions)		
+			
+ 
+ 
+ 
+ */
+
 namespace ContactManager
 {
     // Define the Contact class as the base class for both individuals and companies
@@ -34,8 +69,5 @@ namespace ContactManager
             public string Title { get; set; }
         }
     }
-
-
-   
 
 }
---
+++ assignment8/ContactManagerConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ContactManagerConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
