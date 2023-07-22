3d53711a25a2067fe71fad5ba2c9ae039ee04d92
Rayna Huang <raynainca190217@gmail.com>
2023-07-22 07:11:13 -0700

    update

--- assignment8/Models/Company.cs
+++ assignment8/Models/Company.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Abstractions;
 
 namespace Models
 {
-    internal class Company
+    public class Company : ICompany
     {
+        public string Id { get; set; } // You can generate a unique ID for each company, e.g., using Guid.NewGuid().ToString()
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<ContactDetail> ContactDetails { get; set; } // Include ContactDetails here
+        public List<ContactDetail> PhoneNumbers { get; set; }
+        public List<ContactDetail> Emails { get; set; }
+        public List<IIndividual> Employees { get; set; }
+
+        public Company()
+        {
+            ContactDetails = new List<ContactDetail>();
+            PhoneNumbers = new List<ContactDetail>();
+            Emails = new List<ContactDetail>();
+            Employees = new List<IIndividual>();
+        }
     }
 }
--- assignment8/Models/Individual.cs
+++ assignment8/Models/Individual.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Abstractions;
 
 namespace Models
 {
-    internal class Individual
+    public class Individual : IIndividual
     {
+        public string Id { get; set; } // You can generate a unique ID for each individual, e.g., using Guid.NewGuid().ToString()
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<ContactDetail> ContactDetails { get; set; } // Include ContactDetails here
+        public List<ContactDetail> PhoneNumbers { get; set; }
+        public List<ContactDetail> Emails { get; set; }
+        public ICompany Employer { get; set; }
+
+        public Individual()
+        {
+            ContactDetails = new List<ContactDetail>();
+            PhoneNumbers = new List<ContactDetail>();
+            Emails = new List<ContactDetail>();
+        }
     }
 }
